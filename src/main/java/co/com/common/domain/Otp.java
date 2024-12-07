0afa375c8e4f44bdab6714e308dc67c508e34f29
Nicolas <nicolas@dlpbasculas.com>
2024-12-07 03:01:54 -0500

    Feature: Add OTP entity, repository, mapper, and exception handling for password recovery

---
+++ src/main/java/co/com/common/domain/Otp.java
@@ -0,0 +1,35 @@
+package co.com.common.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "otps")
+public class Otp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String otp;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/co/com/common/exceptions/InvalidOtpException.java
@@ -0,0 +1,7 @@
+package co.com.common.exceptions;
+
+public class InvalidOtpException extends RuntimeException {
+    public InvalidOtpException(String message) {
+        super(message);
+    }
+}
