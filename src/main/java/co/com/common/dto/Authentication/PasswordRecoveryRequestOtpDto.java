accfe1b48e3325d6d3c78cf190769521008fd07d
Nicolas <nicolas@dlpbasculas.com>
2024-12-07 02:58:23 -0500

    Feature: Implement OTP-based password recovery with validation and exception handling

--- src/main/java/co/com/common/dto/Authentication/PasswordRecoveryRequestOtpDto.java
+++ src/main/java/co/com/common/dto/Authentication/PasswordRecoveryRequestOtpDto.java
@@ -1,5 +1,6 @@
 package co.com.common.dto.Authentication;
 
+import co.com.common.utils.validations.AuthenticationValidate;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -9,8 +10,8 @@ import lombok.Data;
 @AllArgsConstructor
 @Builder
 @Data
-public class PasswordRecoveryRequestDto {
-    @Email
-    @NotBlank
+public class PasswordRecoveryRequestOtpDto {
+    @NotBlank(message = AuthenticationValidate.EMAIL_NOT_EMPTY)
+    @Email(message = AuthenticationValidate.EMAIL_NOT_VALID)
     private String email;
 }
---
+++ src/main/java/co/com/common/exceptions/OtpNotFoundException.java
@@ -0,0 +1,7 @@
+package co.com.common.exceptions;
+
+public class OtpNotFoundException extends RuntimeException {
+  public OtpNotFoundException(String message) {
+    super(message);
+  }
+}
