feb0b94bd21fafb8158ae65ee7680f59f343d919
Nicolas <nicolas@dlpbasculas.com>
2024-12-07 21:55:47 -0500

    Fix: Change OTP parameter type from String to Integer in findByOtpAndCredential method

--- src/main/java/co/com/common/repository/ForgotPasswordRepository.java
+++ src/main/java/co/com/common/repository/ForgotPasswordRepository.java
@@ -11,5 +11,5 @@ import org.springframework.data.jpa.repository.Query;
 public interface ForgotPasswordRepository extends JpaRepository<ForgotPassword, Integer>{
 
   @Query("SELECT f FROM ForgotPassword f WHERE f.otp = ?1 AND f.credential = ?2")
-  Optional<ForgotPassword> findByOtpAndCredential(String otp, Credential credential);
+  Optional<ForgotPassword> findByOtpAndCredential(Integer otp, Credential credential);
 }
