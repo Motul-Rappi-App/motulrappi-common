ab80dd3dfae5fbdc4f7dbe7c3442a3c2229f8483
Nicolas <nicolas@dlpbasculas.com>
2024-12-07 21:03:28 -0500

    Enhancement: Add EMAIL_SENT message for email sending validation in CredentialValidate

--- src/main/java/co/com/common/utils/validations/CredentialValidate.java
+++ src/main/java/co/com/common/utils/validations/CredentialValidate.java
@@ -6,7 +6,10 @@ public class CredentialValidate {
 
     public static final String EMAIL_NOT_EMPTY = "El correo electrónico de la credencial no puede ser nulo o vacío";
     public static final String EMAIL_NOT_VALID = "El correo electrónico de la credencial no es válido";
+
     public static final String EMAIL_ALREADY_EXISTS = "El correo electrónico de la credencial %s ya existe";
+
+    public static final String EMAIL_SENT = "El correo electrónico ha sido enviado";
 
     public static final String PASSWORD_NOT_EMPTY = "La contraseña de la credencial no puede ser nula o vacía";
     public static final String PASSWORD_NOT_VALID = "La contraseña de la credencial no es válida";
