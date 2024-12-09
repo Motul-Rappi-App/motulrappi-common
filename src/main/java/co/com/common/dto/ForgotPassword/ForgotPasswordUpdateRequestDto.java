cf0002dbec976e78eef89b59269f6c138a70fa32
Nicolas <nicolas@dlpbasculas.com>
2024-12-08 20:11:30 -0500

    Refactor: Update validation messages in ForgotPasswordUpdateRequestDto and enhance ForgotPasswordMapper with new mapping methods

--- src/main/java/co/com/common/dto/ForgotPassword/ForgotPasswordUpdateRequestDto.java
+++ src/main/java/co/com/common/dto/ForgotPassword/ForgotPasswordUpdateRequestDto.java
@@ -1,25 +1,22 @@
 package co.com.common.dto.ForgotPassword;
 
-import co.com.common.utils.validations.CredentialValidate;
+import co.com.common.utils.validations.AuthenticationValidate;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
-
-@AllArgsConstructor
 @Builder
 @Data
 public class ForgotPasswordUpdateRequestDto {
 
-  @NotBlank(message = CredentialValidate.PASSWORD_NOT_EMPTY)
-  @Pattern(regexp = CredentialValidate.PASSWORD_REGEX, message = CredentialValidate.PASSWORD_NOT_VALID)
+  @NotBlank(message = AuthenticationValidate.PASSWORD_NOT_EMPTY)
+  @Pattern(regexp = AuthenticationValidate.PASSWORD_REGEX, message = AuthenticationValidate.PASSWORD_NOT_VALID)
   private String password;
 
-  @NotBlank(message = CredentialValidate.PASSWORD_NOT_EMPTY)
-  @Pattern(regexp = CredentialValidate.PASSWORD_REGEX, message = CredentialValidate.PASSWORD_NOT_VALID)
+  @NotBlank(message = AuthenticationValidate.PASSWORD_NOT_EMPTY)
+  @Pattern(regexp = AuthenticationValidate.PASSWORD_REGEX, message = AuthenticationValidate.PASSWORD_NOT_VALID)
   private String confirmPassword;
 
 }
--- src/main/java/co/com/common/mappers/ForgotPasswordMapper.java
+++ src/main/java/co/com/common/mappers/ForgotPasswordMapper.java
@@ -1,8 +1,37 @@
 package co.com.common.mappers;
 
-import co.com.common.domain.ForgotPassword;
 import co.com.common.dto.ForgotPassword.ForgotPasswordRequestDto;
+import co.com.common.dto.ForgotPassword.ForgotPasswordResponseDto;
+import co.com.common.dto.ForgotPassword.ForgotPasswordUpdateRequestDto;
+import co.com.common.domain.Credential;
+import co.com.common.domain.ForgotPassword;
+
+import java.util.Date;
 
 public class ForgotPasswordMapper {
 
+    public static ForgotPasswordResponseDto toResponseDto(String message) {
+        return new ForgotPasswordResponseDto(message);
+    }
+
+    public static ForgotPassword toForgotPassword(Credential credential, int otp) {
+        return ForgotPassword.builder()
+                .otp(otp)
+                .expirationTime(new Date(System.currentTimeMillis() + 5 * 60 * 1000))
+                .credential(credential)
+                .build();
+    }
+
+    public static Credential toCredential(ForgotPasswordRequestDto requestDto) {
+      return Credential.builder()
+              .email(requestDto.getEmail())
+              .build();
+    }
+
+    public static ForgotPasswordUpdateRequestDto toUpdateRequest(String password, String confirmPassword) {
+      return ForgotPasswordUpdateRequestDto.builder()
+              .password(password)
+              .confirmPassword(confirmPassword)
+              .build();
+    }
 }
