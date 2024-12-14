dcdbe5ee6a5dc6e3e6f144c873547846251ebd7c
Andres Mauricio <AndyBoni_2012@hotmail.com>
2024-12-13 23:47:09 -0500

    Feature: Date format

--- src/main/java/co/com/common/utils/constants/ExcelConstants.java
+++ src/main/java/co/com/common/utils/constants/ExcelConstants.java
@@ -1,5 +1,7 @@
 package co.com.common.utils.constants;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ExcelConstants {
     public static final String REPORTS_FILE_NAME = "REPORTES";
@@ -7,4 +9,9 @@ public class ExcelConstants {
     public static final String RAPPI_TENDEROS_INSCRITOS = "RAPPI TENDEROS INSCRITOS (%s)";
     public static final String RAPPI_TENDEROS_REDIMIDOS = "RAPPI TENDEROS REDIMIDOS (%s)";
     public static final String SOAT_REDIMIDOS = "SOAT REDIMIDOS (%s)";
+
+    public static final String DATE_FORMAT = "HH:mm:ss dd/MM/yyyy";public static String formatDate(Date date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+        return dateFormat.format(date);
+    }
 }
