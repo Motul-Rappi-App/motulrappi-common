74204892871761fc6a43531ec662f6d63d8b09d1
Andres Mauricio <AndyBoni_2012@hotmail.com>
2024-12-13 20:41:36 -0500

    Feature: Excel validation

--- src/main/java/co/com/common/utils/validations/AdminValidate.java
+++ src/main/java/co/com/common/utils/validations/AdminValidate.java
@@ -22,4 +22,5 @@ public class AdminValidate {
     public static final String SUPER_ADMIN_NOT_SUPPORTED = "El administrador es superadministrador y no puede acceder a este método";
     public static final String NO_SUPER_ADMIN_NOT_SUPPORTED = "El administrador no es superadministrador y no puede acceder a este método";
 
+    public static final String REPORT_LOCATION_NOT_FOUND = "La ubicación con ID %s no fue encontrada";
 }
