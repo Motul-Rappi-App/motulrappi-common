81d74ed85357ac00be932b9b1940542beabf702b
Andres Mauricio <AndyBoni_2012@hotmail.com>
2024-12-13 21:44:55 -0500

    Fix: Minor words have been changed

--- src/main/java/co/com/common/utils/constants/ExcelConstants.java
+++ src/main/java/co/com/common/utils/constants/ExcelConstants.java
@@ -4,7 +4,7 @@ package co.com.common.utils.constants;
 public class ExcelConstants {
     public static final String REPORTS_FILE_NAME = "REPORTES";
 
-    public static final String RAPPI_TENDEROS_INSCRITOS = "RAPPI TENDEROS INSCRITOS (LOCATION %s)";
-    public static final String RAPPI_TENDEROS_REDIMIDOS = "RAPPI TENDEROS REDIMIDOS (LOCATION %s)";
-    public static final String SOAT_REDIMIDOS = "SOAT REDIMIDOS (LOCATION %s)";
+    public static final String RAPPI_TENDEROS_INSCRITOS = "RAPPI TENDEROS INSCRITOS (%s)";
+    public static final String RAPPI_TENDEROS_REDIMIDOS = "RAPPI TENDEROS REDIMIDOS (%s)";
+    public static final String SOAT_REDIMIDOS = "SOAT REDIMIDOS (%s)";
 }
