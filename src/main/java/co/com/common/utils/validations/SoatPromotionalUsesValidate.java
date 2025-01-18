57fd58512b6b2a82badd9bc1b709163a4957759a
Jurgen212 <jurgensanclemente@gmail.com>
2025-01-17 19:11:25 -0500

    Prueba

--- src/main/java/co/com/common/utils/validations/SoatPromotionalUsesValidate.java
+++ src/main/java/co/com/common/utils/validations/SoatPromotionalUsesValidate.java
@@ -2,15 +2,15 @@ package co.com.common.utils.validations;
 
 
 public class SoatPromotionalUsesValidate {
-    public static final String ID_NOT_NULL = "El ID del uso de la promoción para SOAT no puede ser nulo";
+    public static final String ID_NOT_NULL = "El ID del uso de la promoción con SOAT no puede ser nulo";
 
     public static final String COMMERCE_NOT_NULL = "El comercio de la promoción para SOAT no puede ser nulo";
     public static final String COMMERCE_NOT_FOUND = "El comercio de la promoción para SOAT con ID %s no fue encontrado";
 
-    public static final String IDENTIFICATION_NOT_EMPTY = "La identificación de la promoción para SOAT no puede estar vacía";
-    public static final String IDENTIFICATION_ALREADY_USED = "La identificación con ID %s ya fue utilizada para promoción";
+    public static final String IDENTIFICATION_NOT_EMPTY = "La identificación de la promoción con SOAT no puede estar vacía";
+    public static final String IDENTIFICATION_ALREADY_USED = "El código con ID %s ya fue utilizada para promoción";
 
-    public static final String QUANTITY_LITERS_NOT_NULL = "La cantidad de litros de la promoción para SOAT no puede ser nula";
+    public static final String QUANTITY_LITERS_NOT_NULL = "La cantidad de litros de la promoción para consumidor final no puede ser nula";
     public static final String QUANTITY_LITERS_NOT_EMPTY = "El campo quantityLiters no puede ser nulo o vacío";
     public static final String QUANTITY_LITERS_INVALID = "El campo quantityLiters es inválido (debe ser 1 o 2)";
 
--- src/main/java/co/com/common/utils/validations/ValidatePromotionValidation.java
+++ src/main/java/co/com/common/utils/validations/ValidatePromotionValidation.java
@@ -4,19 +4,19 @@ import java.time.LocalDateTime;
 
 
 public class ValidatePromotionValidation {
-    public static final String IDENTIFICATION_NOT_EMPTY = "La identificación no puede ser nula o vacía";
+    public static final String IDENTIFICATION_NOT_EMPTY = "La identificación o código no puede ser nulo o vacío";
 
-    public static final String IDENTIFICATION_NOT_FOUND = "El número de cédula %s no está registrado para promoción, ¿Se trata de un Rappi Tendero? ¡Inscríbelo!";
+    public static final String IDENTIFICATION_NOT_FOUND = "El número de cédula o código %s no está registrado para promoción, ¿Se trata de un Rappi Tendero? ¡Inscríbelo!";
 
-    public static final String SOAT_PROMOTION_VALID = "Felicitaciones, al consumidor final; la cédula es apta para obtener un descuento del 20%%.";
-    public static final String SOAT_PROMOTION_INVALID_FOR_USE = "Esta cédula ya fue redimida por el consumidor final; lastimosamente, no aplica el descuento.";
+    public static final String SOAT_PROMOTION_VALID = "Felicitaciones, al consumidor final; el código es apto para obtener un descuento del 20%%";
+    public static final String SOAT_PROMOTION_INVALID_FOR_USE = "Este código ya fue redimido por el consumidor final; lastimosamente, no aplica el descuento";
     public static final String SOAT_PROMOTION_TIME_LIMIT_INVALID = "La promoción para consumidores finales ha caducado.";
-    public static final LocalDateTime SOAT_PROMOTION_TIME_LIMIT = LocalDateTime.of(2025, 3, 30, 0, 0);
+    public static final LocalDateTime SOAT_PROMOTION_TIME_LIMIT = LocalDateTime.of(2025, 4, 30, 0, 0);
 
     public static final long RT_PROMOTION_DAYS_MIN = 10;
     public static final String RT_PROMOTION_VALID_FIRST_USE = "Felicitaciones al Rappi Tendero por su primera vez usando la promoción. Es apto para recibir un descuento del 10%%";
-    public static final String RT_PROMOTION_VALID_WITH_TIME = "Felicitaciones, aplica para la promoción un descuento del 10%%. Rappi Tendero puede redimirla de nuevo en %s días.";
-    public static final String RT_PROMOTION_INVALID_WITH_TIME = "No aplica para promoción; no han pasado %s días desde el último uso, la última vez fue el día %s.";
+    public static final String RT_PROMOTION_VALID_WITH_TIME = "Felicitaciones, aplica para la promoción un descuento del 10%%. Rappi Tendero puede redimirla de nuevo en %s días";
+    public static final String RT_PROMOTION_INVALID_WITH_TIME = "No aplica para promoción; no han pasado %s días desde el último uso, la última vez fue el día %s";
     public static final String RT_PROMOTION_ERROR_WITH_DATE = "Error al obtener fechas del uso de la promoción";
 
     public static final String SAVE_IN_RT_PROMOTION_USES = "RT_PROMOTION_USES";
