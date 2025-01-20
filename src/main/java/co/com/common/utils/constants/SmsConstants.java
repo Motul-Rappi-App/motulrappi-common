ac1f09d11b204e76bce65e18ee803004d099d006
Jurgen212 <jurgensanclemente@gmail.com>
2025-01-20 17:26:06 -0500

    Finish sms

--- src/main/java/co/com/common/utils/constants/SmsConstants.java
+++ src/main/java/co/com/common/utils/constants/SmsConstants.java
@@ -9,6 +9,6 @@ import lombok.Data;
 @Data
 public class SmsConstants {
     public static final String API_KEY = "3b04ad2bb8d307596d4cf4b44c9f4cbd-ec8e727f-a5ad-4499-ac87-f1d97585f856";
-    public static final String INSCRIPTION_MESSAGE = "Bienvenido a la alianza MotulRappi. Aprovecha el 10% de descuento en aceites Motul todo el 2025. Conoce los puntos de venta en https://www.motul.com/es-CO/discover-motul/motul-es-mas.";
-    public static final String REDEMPTION_MESSAGE = "REDEMPTION SUCCESFULL";
+    public static final String INSCRIPTION_MESSAGE = "Bienvenido a la alianza MotulRappi. Aprovecha el 10% de descuento en aceites Motul todo el 2025.";
+    public static final String REDEMPTION_MESSAGE = "¡Felicitaciones, amigo Rappi Tendero! Has redimido exitosamente tu descuento del 10%. Recuerda que puedes usarlo durante todo el 2025.";
 }
