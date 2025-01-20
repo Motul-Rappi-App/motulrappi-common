d32bfdce98581ea89b42ea3a5d9195edcc53c40f
Jurgen212 <jurgensanclemente@gmail.com>
2025-01-20 16:23:37 -0500

    SMS started

---
+++ src/main/java/co/com/common/utils/constants/SmsConstants.java
@@ -0,0 +1,12 @@
+package co.com.common.utils.constants;
+
+public class SmsConstants {
+    private static final String API_KEY = "3b04ad2bb8d307596d4cf4b44c9f4cbd-ec8e727f-a5ad-4499-ac87-f1d97585f856";
+
+    public String getApiKey(){
+        return API_KEY;
+    }
+
+    public static final String INSCRIPTION_MESSAGE = "INSCRIPTION SUCCESFULL";
+    public static final String REDEMPTION_MESSAGE = "REDEMPTION SUCCESFULL";
+}
